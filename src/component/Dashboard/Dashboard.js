517d61db3603a19284a45d9c3649545b00f8767b
Suraj kumar <surajkumar54985@gmail.com>
2022-06-22 16:48:43 +0530

    Centralised the card in mobile view

---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const DashBoard = () => {
+  return (
+    <div className="cardHolder">
+      <div className="signup-form" style={{ backgroundColor: "#19aa8d" }}>
+        DashBoard page......
+      </div>
+    </div>
+  );
+};
+
+
+export default DashBoard;
